f34d65f40a6b813033f903916d8738f3943c7542
prosper_julius_mavunga <prmavhunga@gmail.com>
2021-01-02 23:31:22 +0100

    initial commint

---
+++ src/test/java/com/binaryparlour/apiemployeemanagement/ApiEmployeeManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.binaryparlour.apiemployeemanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiEmployeeManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
